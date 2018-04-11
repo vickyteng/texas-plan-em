f0cc4ba36166af208c8e6bf205293e6fea16b01d
Sinh Nguyen <sinh.nguyen@libertymutual.com%>
2018-04-11 14:17:17 -0400

    Users & UserList components

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import StartSession from 'components/StartSession';
+import Users from 'components/Users';
 
 const AppRouter = () => (
     <BrowserRouter>
         <Switch>
             <Route exact path="/" component={StartSession} />
+            <Route exact path="/users" component={Users} />
         </Switch>
     </BrowserRouter>
 );
