8b7f757971fb08a6b80c355f92f1358e3972400c
Christopher Marcenelle <christopher.marcenelle@libertymutual.com>
2018-04-11 15:58:09 -0400

    Add Routes and scss

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import StartSession from 'components/StartSession';
+import Game from 'components/Game';
+
 
 const AppRouter = () => (
     <BrowserRouter>
         <Switch>
             <Route exact path="/" component={StartSession} />
+            <Route exact path="/game/:id" component={Game} />
         </Switch>
     </BrowserRouter>
 );
