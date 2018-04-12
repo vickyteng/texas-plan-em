defde5cb2e4aa7a97737123fb64e3564e030bff9
Juan Flores <juan.flores@libertymutual.com>
2018-04-12 15:45:48 -0400

    respond to firebase user events

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import CreateSession from 'components/CreateSession';
-import Game from 'components/Game';
 import Users from 'components/users/Users'
+import GameContainter from 'components/GameContainter';
 
 
 const AppRouter = () => (
     <BrowserRouter>
         <Switch>
             <Route exact path="/" component={CreateSession} />
-            <Route exact path="/game/:id" component={Game} />
+            <Route exact path="/game/:id" component={GameContainter} />
             <Route exact path="/users" component={Users} />
         </Switch>
     </BrowserRouter>
--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -2,13 +2,15 @@ import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import sessionReducer from 'reducers/sessionReducer';
 import userReducer from 'reducers/userReducer';
 import thunk from 'redux-thunk';
+import gameReducer from 'reducers/gameReducer';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 export default () => {
     const store = createStore(combineReducers({
             session: sessionReducer,
-            user: userReducer
+            user: userReducer,
+            game: gameReducer,
         }),
         composeEnhancers(applyMiddleware(thunk))
     );
