718a18729c3f877760304060f9d9671b4de7e451
Matt Wong <matthew067d.wong@libertymutual.com>
2018-04-13 11:02:14 -0400

    add functionality to join session button and update unit tests

--- src/components/CreateSession.js
+++ src/components/CreateSession.js
@@ -10,125 +10,6 @@ import SessionUrl from 'components/SessionUrl';
 
 export const CreateSession = (props) => (
     <div className="create-session">
-        {/* <div className="app-title">
-            <h1>Texas Plan 'em</h1>
-        </div> */}
-        {/*
-        <div className="session-card">
-            <div className="session-card-title">
-                <h3>Create New Game</h3>
-            </div>
-            <div className="session-card-body">
-                <Row>
-                    <Col>
-                        <Form
-                            onSubmit={
-                                (e) => {
-                                    e.preventDefault();
-
-                                    if (!props.session && props.name) {
-                                        props.startStartSession(props.name);
-                                    }
-                                }
-                            }
-                        >
-                            <FormGroup>
-                                <InputGroup>
-                                    <Input
-                                        placeholder="Name"
-                                        name="name"
-                                        id="name"
-                                        onChange={e => props.editUser({name: e.target.value})}
-                                        value={props.name || ''}
-                                    />
-                                    <InputGroupAddon addonType="append">
-                                        <InputGroupText>
-                                            <FontAwesome name="user" fixedWidth={true} />
-                                        </InputGroupText>
-                                    </InputGroupAddon>
-                                </InputGroup>
-                            </FormGroup>
-                            <SessionUrl session={props.session} />
-                            {
-                                props.session &&
-                                <hr/>
-                            }
-                            {
-                                props.session &&
-                                <FormGroup tag="fieldset">
-                                    <legend>Are you Participating or Observing?</legend>
-                                    <FormGroup check>
-                                        <Input
-                                        type="radio"
-                                        name="user-type"
-                                        id="participant"
-                                        checked={props.role === 'Participant'}
-                                        onChange={() => props.editUser({role: 'Participant'})}
-
-                                        />
-                                        <Label check for="participant">
-                                        Participant
-                                        </Label>
-                                    </FormGroup>
-                                    <FormGroup check>
-                                        <Input
-                                        type="radio"
-                                        name="user-type"
-                                        id="observer"
-                                        checked={props.role === 'Observer'}
-                                        onChange={() => props.editUser({role: 'Observer'})}
-                                        />
-                                        <Label check for="observer">
-                                        Observer
-                                        </Label>
-                                    </FormGroup>
-                                </FormGroup>
-                            }
-                        </Form>
-                    </Col>
-                </Row>
-            </div>
-            <div className="session-card-footer">
-                <Row>
-                    <Col>
-                        {
-                            props.session
-                            ?
-                            <Button
-                                id="join-session"
-                                block
-                                color="inverse"
-                                disabled={!props.name || !props.role}
-                            >
-                                Join Session
-                            </Button>
-                            :
-                            <Button
-                                id="start-session"
-                                onClick={() => props.startStartSession(props.name)}
-                                block
-                                color="inverse"
-                                disabled={!props.name}
-                            >
-                                Start Session
-                            </Button>
-                        }
-                    </Col>
-                </Row>
-            </div>
-        </div>
-                    */}
-        {/* <Card className="session-card">
-            <CardTitle title="Create New Game" />
-            <CardText>
-                <TextField
-                    floatingLabelText="Name"
-                    fullWidth={true}
-                    value={props.name || ''}
-                    onChange={e => props.editUser({name: e.target.value})}
-                />
-            </CardText>
-        </Card> */}
         <div className="card create-session-outer">
             <div className="top">
                 <h1>Texas Plan 'em</h1>
@@ -136,6 +17,7 @@ export const CreateSession = (props) => (
             <div className="bottom">
                 <div className="card create-session-inner">
                     <TextField
+                        id="name"
                         floatingLabelText="Name"
                         fullWidth={true}
                         value={props.name || ''}
@@ -144,6 +26,7 @@ export const CreateSession = (props) => (
                     {
                         props.session &&
                         <TextField
+                            id="url"
                             floatingLabelText="URL"
                             fullWidth={true}
                             value={props.session ? `${window.location.origin}/game/${props.session}` : ''}
@@ -173,6 +56,7 @@ export const CreateSession = (props) => (
                             id="join-session"
                             color="inverse"
                             disabled={!props.name || !props.role}
+                            onClick={() => props.history.push(`game/${props.session}`)}
                         >
                             Join Session
                         </button>
